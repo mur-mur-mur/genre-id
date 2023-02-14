4e2a30ddc24f30b27360cbf69dcf1464df6356b1
adam robson <adamray312@gmail.com>
2023-02-14 13:14:07 -0800

    finish the controller

--- server/lib/controllers/users.js
+++ server/lib/controllers/users.js
@@ -1,5 +1,10 @@
 const { Router } = require('express');
 const UserService = require('../models/UserService');
+
+const COOKIE_NAME = process.env.COOKIE_NAME;
+const SECURE_COOKIES = process.env.SECURE_COOKIES;
+const ONE_DAY_IN_MS = 1000 * 60 * 60 * 24;
+
 // eslint-disable-next-line new-cap
 module.exports = Router()
 
@@ -14,4 +19,23 @@ module.exports = Router()
       next(e);
    
     }
+  })
+
+  .post('/users/go', async (req, res, next) => {
+    try {
+      const token = await UserService.signIn(req.body);
+
+      res
+        .cookie(COOKIE_NAME, token, {
+          httpOnly: true,
+          secure: SECURE_COOKIES === 'true',
+          sameSite: SECURE_COOKIES === 'true' ? 'none' : 'strict',
+          maxAge: ONE_DAY_IN_MS })
+          
+        .json({ message: 'You have been signed in successfully!' });
+     
+    } catch (e) {
+      next(e);
+    }
   });
+
