959637fcf774c34da70811310a4c426bb569c9a0
adam robson <adamray312@gmail.com>
2023-02-14 13:01:05 -0800

    build out user service

--- server/lib/services/UserService.js
+++ server/lib/services/UserService.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
+const JWT_SECRET = process.env.JWT_SECRET;
 const SALT_ROUNDS = process.env.SALT_ROUNDS;
 
 module.exports = class UserService {
@@ -19,5 +21,29 @@ module.exports = class UserService {
     });
 
     return user;
+  }
+
+
+  static async signIn({ email, password = '' }) {
+    try {
+
+      const user = await User.getByEmail(email);
+
+      if (!user) throw new Error('That email is invalid. Please try again');
+
+      if (!bcrypt.compareSync(password, user.passwordHash))
+        throw new Error('That password is invalid. Please try again');
+
+      const token = jwt.sign({ ...user }, JWT_SECRET, {
+        expiresIn: '1 day'
+      });
+      return token;
+
+    } catch (e) {
+      
+      e.status = 401;
+      throw e;
+    
+    }
   }
 };
