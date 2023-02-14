53af52ac9da78dff3a8fd9507d1bfff6fbd5c8d4
adam robson <adamray312@gmail.com>
2023-02-14 12:14:50 -0800

    setup pool.js

---
+++ server/controllers/users.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+
+// eslint-disable-next-line new-cap
+module.exports = Router()
+
+  .post('/users', async (req, res, next) => {
+    
+  });
---
+++ server/utils/pool.js
@@ -0,0 +1,10 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.PGSSLMODE && { rejectUnauthorized: false },
+});
+
+pool.on('connect', () => console.info('🐘🐘🐘🐘🐘 Postgres connected 🐘🐘🐘🐘🐘'));
+
+module.exports = pool;
