232acf054de2b31f84ab3a9d4b057415c2c7522c
adam robson <adamray312@gmail.com>
2023-02-15 12:29:29 -0800

    add canvas min canvas react write handle submit

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { GENRES } from '../../services/genres';
-
+const DATABASE_URL = process.env.DATABASE_URL;
 
 export default function nu({  
   genresList, 
@@ -12,26 +12,21 @@ export default function nu({
     const form = e.target;
     const data = new FormData(form);
     
-    fetch('/#', { method: form.method, body: data });
+    fetch(DATABASE_URL, { method: form.method, body: data });
     
     return 'the following genres have been added: ' + [...data.entries()].join(' ');
-  };
-// eslint-disable-next-line no-unused-vars
-  const getList = () => {
-    const list = document.getElementBbyId('list');
-    const form = document.getElementById('form');
-    let nuArray = [];
-    const items = list.children;
-    for (let item of items) {
-      nuArray.push(item);
-    }
-    form.append(nuArray);
-    return form;
-  };
+  };  
 
   return (
     <>
       <section className="form-container">
+        <article className="list-container">
+          <ul id="list" className="list">{
+            genresList.map((genre, index) => {
+              return <li className="list-item" key={ index }>{ genre }</li>;
+            })
+          }</ul>
+        </article>
         <form id="form" method="post" onSubmit={ handleSubmit }>
           <article className="form-select">
             <label>genres<br />
@@ -51,13 +46,7 @@ export default function nu({
                 }</select>
             </label>
           </article>
-          <article className="list-container">
-            <ul id="list" className="list">{
-              genresList.map((genre, index) => {
-                return <li key={ index }>{ genre }</li>;
-              })    
-            }</ul>
-          </article>
+          
         </form>
       </section>
     </>
