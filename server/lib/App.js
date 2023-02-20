886c35cc291765698480d04182ca3972381ad10b
adam robson <adamray312@gmail.com>
2023-02-20 01:51:09 -0800

    editor config

--- server/lib/App.js
+++ server/lib/App.js
@@ -1,11 +1,20 @@
+const createError = require('http-errors');
 const express = require('express');
+const path = require('path');
 const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+
 const app = express();
-
 app.use(express.json());
-app.use(cookieParser());
 
+app.use(cookieParser());
+app.use(helmet());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', require('./controllers/index'));
 app.use('/users', require('./controllers/users'));
+app.use('/genres', require('./controllers/users'));
 
 app.use(require('./middleware/not-found'));
 app.use(require('./middleware/error'));
