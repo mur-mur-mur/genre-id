a7e5846945d46f248139641cf5228920bf74f1f4
adam robson <adamray312@gmail.com>
2023-02-19 21:20:13 -0800

    fix the genres constant

--- src/services/genres.jsx
+++ src/services/genres.jsx
@@ -1,9 +1,178 @@
 export const GENRES = [
-  'classical',
-  'avant-garde',
-  'popular',
-  'regional',
-  'religious',
-  'folk',
-  'miscellaneous'
+  {
+    id: 1,
+    title: 'Pop music',
+    subgenre: '',
+    description: 'A genre of popular music that originated in the West during the 1950s and 1960s.Pop music is eclectic, often borrowing elements from urban, dance, rock, Latin, country, and other styles.Songs are typically short to medium - length with repeated choruses, melodic tunes, and hooks.',
+    comment: '8,813 annotations in dataset'
+  },
+  {
+    id: 2,
+    title: 'Hip Hop music',
+    subgenre: '',
+    description: 'Hip hop or rap music formed in the United States in the 1970s and consists of stylized rhythmic music that commonly accompanies rhythmic and rhyming speech("rapping").',
+    comment: '7,745 annotations in dataset'
+  },
+  {
+    id: 3,
+    title: 'Rock music',
+    subgenre: '',
+    description: 'A genre of popular music that originated as "rock and roll" in the United States in the 1950s, and developed into a range of different styles in the 1960s and later.Compared to pop music, rock places a higher degree of emphasis on musicianship, live performance, and an ideology of authenticity.',
+    comment: '8,475 annotations in dataset',
+  },
+  {
+    id: 4,
+    title: 'Rhythm & Blues music',
+    subgenre: '',
+    description: 'A genre of popular African-American music that originated in the 1940s as urbane, rocking, jazz based music with a heavy, insistent beat.  Lyrics focus heavily on the themes of triumphs and failures in terms of relationships, freedom, economics, aspirations, and sex.',
+    comment: '5,133 annotations in dataset',
+  },
+  {
+    id: 5,
+    title: ' Soul music',
+    subgenre: '',
+    description: 'A popular music genre that combines elements of African - American gospel music, rhythm and blues and jazz.',
+    comment: '2,930 annotations in dataset'
+  },
+  {
+    id: 6,
+    title: 'Reggae music',
+    subgenre: '',
+    description: 'A music genre that originated in Jamaica in the late 1960s, strongly influenced by traditional mento as well as American jazz and rhythm and blues, instantly recognizable from the counterpoint between the bass and drum downbeat, and the offbeat rhythm section.',
+    comment: '3,229 annotations in dataset'
+  },
+  {
+    id: 7,
+    title: 'Country music',
+    subgenre: '',
+    description: 'A genre of United States popular music with origins in folk, Blues and Western music, often consisting of ballads and dance tunes with generally simple forms and harmonies accompanied by mostly string instruments such as banjos, electric and acoustic guitars, dobros, and fiddles as well as harmonicas.',
+    comment: '5,530 annotations in dataset',
+  },
+  {
+    id: 8,
+    title: 'Funk music',
+    subgenre: '',
+    description: 'A music genre that originated in the 1960s when African American musicians created a rhythmic, danceable new form of music that de - emphasized melody and chord progressions to bring a strong rhythmic groove of a bass line and drum part to the foreground.',
+    comment: '4,065 annotations in dataset'
+  },
+  {
+    id: 9,
+    title: 'Folk music',
+    subgenre: '',
+    description: 'A genre that evolved from traditional music during the 20th century folk revival.One meaning often given is that of old songs with no known composers; another is music that has been transmitted and evolved by a process of oral transmission or performed by custom over a long period of time.',
+    comment: '2,220 annotations in dataset'
+  },
+  {
+    id: 10,
+    title: 'Middle Eastern music',
+    subgenre: '',
+    description: 'Music originating from the vast region from Morocco to Iran, including the Arabic countries of the Middle East and North Africa, the Iraqi traditions of Mesopotamia, Iranian traditions of Persia, the Hebrew music of Israel, Armenian music, the varied traditions of Cypriot music, the music of Turkey, traditional Assyrian music, Berbers of North Africa, and Coptic Christians in Egypt.',
+    comment: '2,088 annotations in dataset'
+  },
+  {
+    id: 11,
+    title: 'Jazz music',
+    subgenre: '',
+    description: 'A music genre that originated from African American communities of New Orleans during the late 19th and early 20th centuries in the form of independent traditional and popular musical styles, all linked by the common bonds of African American and European American musical parentage with a performance orientation.',
+    comment: ' 4,930 annotations in dataset'
+  }, 
+  {
+    id: 12,
+    title: 'Disco music',
+    subgenre: '',
+    description: 'A genre of dance music containing elements of funk, soul, pop, and salsa that achieved popularity during the mid - 1970s to the early 1980s.',
+    comment: '4,137 annotations in dataset'
+  },
+  {
+    id: 13,
+    title: 'Classical music',
+    subgenre: '',
+    description: 'Art music produced or rooted in the traditions of Western music, including both liturgical and secular music, over the broad span of time from roughly the 11th century to the present day.',
+    comment: '6,874 annotations in dataset'
+  },
+  {
+    id: 14,
+    title: 'Electronic music',
+    subgenre: '',
+    description: ' A large set of predominantly popular and dance genres in which synthesizers and other electronic instruments are the primary sources of sound.',
+    commment: '38,958 annotations in dataset'
+  },
+  
+  {
+    id: 15,
+    title: 'Music of Latin America',
+    subgenre: '',
+    description: 'Music originating from Latin America which encompasses a wide variety of styles, including son, rumba, salsa, merengue, tango, samba, and bossa nova.',
+    comment: '4,141 annotations in dataset'
+  },
+  {
+    id: 16,
+    title: 'Blues music',
+    subgenre: '',
+    description: 'A genre and musical form developed by African Americans in the United States around the end of the 19th century from African - American work songs and European - American folk music.The blues form, ubiquitous in jazz and rock and roll, is characterized by the call - and - response pattern, the blues scale and specific chord progressions, of which the twelve - bar blues is the most common.Blues shuffles or walking bass reinforce the trance - like rhythm and form a repetitive groove effect.',
+    comment: '4,768 annotations in dataset'
+  },
+  {
+    id: 17,
+    title: 'Music for children',
+    subgenre: '',
+    description: 'Music performed for children, often designed to provide an entertaining means of teaching about cultures, good behavior, facts, and skills.',
+    comment: '3,511 annotations in dataset'
+  },
+  {
+    id: 18,
+    title: 'New-age music',
+    subgenre: '',
+    description: 'A genre of music intended to create artistic inspiration, relaxation, and optimism used by listeners for yoga, massage, meditation, and reading as a method of stress management or to create a peaceful atmosphere.Includes both electronic and acoustic forms.',
+    comment: '3,162 annotations in dataset'
+  },
+  {
+    id: 19,
+    title: 'Vocal music',
+    subgenre: '',
+    description: 'Music performed by one or more singers, with or without instrumental accompaniment, in which singing provides the main focus of the piece.',
+    comment: '3,672 annotations in dataset'
+  },
+  {
+    id: 20,
+    title: 'Music of Africe',
+    subgenre: '',
+    description: 'Music whose style or form clearly indicates African origin or primarily African influence.Given the vastness of the continent, this covers many distinct musical traditions.Sub - Saharan African music frequently relies on percussion instruments including xylophones, drums, and tone - producing instruments such as the mbira or "thumb piano.',
+    comment: '2,907 annotations in dataset'
+  },
+  {
+    id: 21,
+    title: 'Christian music',
+    subgenre: '',
+    description: 'Music that has been written to express either personal or a communal belief regarding Christian life and faith.Its forms vary widely across the world, according to culture and social context.',
+    comment: '3,118 annotations in dataset'
+  },
+  {
+    id: 22,
+    title: 'Music of Asia',
+    subgenre: '',
+    description: 'Musical styles originating from a large number of Asian countries located in Central, Southern, and East Asia.',
+    comment: '3,924 annotations in dataset'
+  },
+  {
+    id: 23,
+    title: 'Ska music',
+    subgenre: '',
+    description: 'A music genre that originated in Jamaica in the late 1950s, combining elements of Caribbean mento and calypso with American jazz and rhythm and blues.It is characterized by a walking bass line accented with rhythms on the off - beat.',
+    comment: '1,722 annotations in dataset'
+  },
+  {
+    id: 24,
+    title: ' Traditional music',
+    subgenre: '',
+    description: 'Musical forms that have origins many generations into the past, commonly without formal notation or description, commonly familiar to people in a given culture.',
+    comment: '1,754 annotations in dataset'
+  },
+  {
+    id: 25,
+    title: 'Independent music',
+    subgenre: '',
+    description: 'Music produced independent of major commercial record labels, possibly including a do -it - yourself approach to recording and publishing.The term indie is also used to describe music of this style regardless of actual production channel.',
+    comment: '3,019 annotations in dataset'
+  }
 ];
